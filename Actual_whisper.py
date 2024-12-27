9cde1cdf614b38ec313d2228c8c3abbc2521621d
Kiran <64211590+Skiran0@users.noreply.github.com>
2024-12-27 16:33:32 +0530

    Update Actual_whisper.py

--- Actual_whisper.py
+++ Actual_whisper.py
@@ -4,7 +4,7 @@ from transformers import pipeline
 
 whisper = pipeline("automatic-speech-recognition", "openai/whisper-large-v3", torch_dtype=torch.float16, device="cuda:0")
 
-transcription = whisper("C:\\Users\\saiki\\Downloads\\Voice 014.mp3")  # replace the mp3 file
+transcription = whisper("C:\\Users\\saiki\\Downloads\\Voice 016.mp3")  # replace the mp3 file
 
 print(transcription["text"])
 
